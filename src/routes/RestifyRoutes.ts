a50d14bb121dcf7451edc8ccd4157bcc9bc561cf
vandbt <sharkspin@gmail.com>
2018-06-08 13:22:44 +0900

    add(restifyRoutes): default limit parameter when get resouce #5

--- src/routes/RestifyRoutes.ts
+++ src/routes/RestifyRoutes.ts
@@ -5,10 +5,33 @@ import * as models from "../models/mongoose";
 
 
 
+const DefaultReadLimit = "50";
+
+/**
+ * Setting Default limit when limit query missed harness over load
+ * @param  {express.Request} req
+ * @param  {express.Response} res
+ * @param  {express.NextFunction} next
+ */
+const paginateMiddleware = function (req: express.Request, res: express.Response, next: express.NextFunction) {
+    // only effect http method GET
+    // if (req.method == "GET") {
+    //     req.query.limit = req.query.limit || DefaultReadLimit;
+    // }
+    req.query.limit = req.query.limit || DefaultReadLimit;
+    // console.log(req.query);
+    next();
+};
+
+// declare option for restify
 const restifyOptions = {
     prefix: "/api",
-    version: "/v1"
+    version: "/v1",
+    totalCountHeader: true
 };
+
+// build base api path
+const routePath = restifyOptions.prefix + restifyOptions.version + "/*";
 
 export class RestifyRoutes {
 
@@ -18,14 +41,18 @@ export class RestifyRoutes {
      * @returns void
      */
     static map(app: express.Application, router: express.Router): void {
+        // use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
         app.use(methodOverride("X-HTTP-Method-Override"));
+
+        router.get(routePath, paginateMiddleware); // apply defulat limit that prevent loading all records.
 
         /**
          * Add mongoose schema models
          */
-        restify.serve(router, models.TodoModel, restifyOptions);
-        restify.serve(router, models.ContactModel, restifyOptions);
-        restify.serve(router, models.LocationModel, restifyOptions);
+        restify.defaults(restifyOptions);
+        restify.serve(router, models.TodoModel);
+        restify.serve(router, models.ContactModel);
+        restify.serve(router, models.LocationModel);
 
         app.use(router);
     }
